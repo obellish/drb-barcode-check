b0b44687f54e347282a71f29034d691f84d004f6
obellish <obellish@mpilabels.com>
2024-01-23 14:55:59 -0500

    work

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,86 @@
+use std::{
+	error::Error as StdError,
+	fmt::{Display, Formatter, Result as FmtResult},
+	ops::{Deref, DerefMut},
+};
 
+use winit::{
+	error::{EventLoopError, OsError},
+	event::DeviceId,
+};
+
+#[derive(Debug)]
+pub enum Error {
+	EventLoop(EventLoopError),
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		match self {
+			Self::EventLoop(e) => Display::fmt(e, f),
+		}
+	}
+}
+
+impl From<EventLoopError> for Error {
+	fn from(value: EventLoopError) -> Self {
+		Self::EventLoop(value)
+	}
+}
+
+impl From<OsError> for Error {
+	fn from(value: OsError) -> Self {
+		Self::EventLoop(value.into())
+	}
+}
+
+impl StdError for Error {
+	fn source(&self) -> Option<&(dyn StdError + 'static)> {
+		match self {
+			Self::EventLoop(e) => Some(e),
+		}
+	}
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug)]
+pub struct Scanner {
+	pub device_id: Option<DeviceId>,
+	buffer: String,
+}
+
+impl Scanner {
+	#[must_use]
+	pub const fn new() -> Self {
+		Self {
+			device_id: None,
+			buffer: String::new(),
+		}
+	}
+
+	#[must_use]
+	pub const fn is_setup(&self) -> bool {
+		self.device_id.is_some()
+	}
+}
+
+impl Default for Scanner {
+	fn default() -> Self {
+		Self::new()
+	}
+}
+
+impl Deref for Scanner {
+	type Target = String;
+
+	fn deref(&self) -> &Self::Target {
+		&self.buffer
+	}
+}
+
+impl DerefMut for Scanner {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.buffer
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use drb_barcode_check::{Result, Scanner};
+use winit::{
+	event::{ElementState, Event, WindowEvent},
+	event_loop::{EventLoop, EventLoopBuilder},
+	keyboard::{Key, NamedKey},
+	platform::{modifier_supplement::KeyEventExtModifierSupplement, windows::DeviceIdExtWindows},
+	window::WindowBuilder,
+};
+
+fn main() -> Result<()> {
+	let event_loop = EventLoop::new()?;
+	let window = WindowBuilder::new().build(&event_loop)?;
+
+	let mut first_scanner = Scanner::new();
+	let mut second_scanner = Scanner::new();
+
+	let mut text = String::new();
+
+	event_loop.run(move |event, elwt| match event {
+		Event::WindowEvent { event, .. } => match event {
+			WindowEvent::CloseRequested => elwt.exit(),
+			WindowEvent::KeyboardInput {
+				event, device_id, ..
+			} => {
+				if event.state == ElementState::Pressed && !event.repeat {
+					match event.logical_key {
+						Key::Character(c) => text.push_str(c.as_str()),
+						Key::Named(named) => {
+							if let Some(t) = named.to_text() {
+								text.push_str(t);
+							}
+						}
+						_ => {}
+					}
+					if text.len() == 14 {
+						println!("{device_id:?}");
+						match (first_scanner.is_setup(), second_scanner.is_setup()) {
+							(false, _) => {
+								first_scanner.device_id = Some(device_id);
+								println!("first scanner setup complete");
+							}
+							(true, false) => {
+								second_scanner.device_id = Some(device_id);
+
+								assert_ne!(
+									first_scanner.device_id, second_scanner.device_id,
+									"same scanner was used twice during setup"
+								);
+								println!("second scanner setup complete");
+								println!("setup complete");
+							}
+							(true, true) => {
+								if first_scanner.device_id == Some(device_id) {
+									first_scanner.push_str(&text);
+								} else {
+									second_scanner.push_str(&text);
+								}
+							}
+						}
+						text.clear();
+					}
+				}
+			}
+			_ => {}
+		},
+		Event::DeviceEvent { device_id, event } => {
+			dbg!(device_id);
+			dbg!(event);
+		}
+		_ => {}
+	})?;
+
+	Ok(())
 }
