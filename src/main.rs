59c272fc4d917c9795139a0a7e5a87bce7d9b8d2
obellish <obellish@mpilabels.com>
2024-01-24 08:18:08 -0500

    this works, switching to egui

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::sync::atomic::{AtomicBool, Ordering};
 
-use drb_barcode_check::{Result, Scanner};
+use drb_barcode_check::Result;
 use winit::{
 	event::{ElementState, Event, WindowEvent},
 	event_loop::{EventLoop, EventLoopBuilder},
@@ -15,66 +15,48 @@ const CONTROL_CODE: &str = "-CONTROL CODE-";
 
 fn main() -> Result<()> {
 	let event_loop = EventLoop::new()?;
-	let window = WindowBuilder::new().build(&event_loop)?;
+	let _window = WindowBuilder::new().build(&event_loop)?;
 
-	let mut first_scanner = Scanner::new();
-	let mut second_scanner = Scanner::new();
+	let mut first_buffer = String::new();
+	let mut second_buffer = String::new();
 
 	let mut text = String::new();
 
-	event_loop.run(move |event, elwt| match event {
-		Event::WindowEvent { event, .. } => match event {
-			WindowEvent::CloseRequested => elwt.exit(),
-			WindowEvent::KeyboardInput {
-				event, device_id, ..
-			} => {
-				if event.state == ElementState::Pressed && !event.repeat {
-					match event.logical_key {
-						Key::Character(c) => text.push_str(c.as_str()),
-						Key::Named(named) => {
-							if let Some(t) = named.to_text() {
-								text.push_str(t);
-							}
-						}
-						_ => {}
-					}
-					if text.len() == 14 {
-						println!("{device_id:?}");
-						match (first_scanner.is_setup(), second_scanner.is_setup()) {
-							(false, _) if text == CONTROL_CODE => {
-								first_scanner.device_id = Some(device_id);
-								println!("first scanner setup complete");
-							}
-							(true, false) if text == CONTROL_CODE => {
-								second_scanner.device_id = Some(device_id);
-
-								assert_ne!(
-									first_scanner.device_id, second_scanner.device_id,
-									"same scanner was used twice during setup"
-								);
-								println!("second scanner setup complete");
-								println!("setup complete");
-							}
-							(true, true) => {
-								if first_scanner.device_id == Some(device_id) {
-									first_scanner.push_str(&text);
-								} else {
-									second_scanner.push_str(&text);
+	event_loop.run(move |event, elwt| {
+		if let Event::WindowEvent { event, .. } = event {
+			match event {
+				WindowEvent::CloseRequested => elwt.exit(),
+				WindowEvent::KeyboardInput {
+					event, device_id, ..
+				} => {
+					if event.state == ElementState::Pressed && !event.repeat {
+						match event.logical_key {
+							Key::Character(c) => text.push_str(c.as_str()),
+							Key::Named(named) => {
+								if let Some(t) = named.to_text() {
+									text.push_str(t);
 								}
 							}
 							_ => {}
 						}
-						text.clear();
+						if text.len() == 14 {
+							if first_buffer.is_empty() {
+								first_buffer.push_str(&text);
+							} else if second_buffer.is_empty() {
+								second_buffer.push_str(&text);
+							}
+							text.clear();
+						}
+
+						if first_buffer.len() == 14 && second_buffer.len() == 14 {
+							first_buffer.clear();
+							second_buffer.clear();
+						}
 					}
 				}
+				_ => std::thread::yield_now(),
 			}
-			_ => {}
-		},
-		Event::DeviceEvent { device_id, event } => {
-			dbg!(device_id);
-			dbg!(event);
 		}
-		_ => {}
 	})?;
 
 	Ok(())
